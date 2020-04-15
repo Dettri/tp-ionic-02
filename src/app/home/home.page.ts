b20a99e3a960a93875fe53d840b44352e574280b
BAUD <alexandre.baud@ynov.com>
2020-04-15 12:42:52 +0200

    ajout section geolocation

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {AlertController} from '@ionic/angular';
 import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @Component({
   selector: 'app-home',
@@ -12,7 +13,7 @@ export class HomePage {
   title: string;
   imgData: string;
 
-  constructor(private alertController: AlertController, private camera: Camera) {}
+  constructor(private alertController: AlertController, private camera: Camera, private geolocation: Geolocation) {}
 
   updateTitle() {
     this.title = 'Mon Nouveau Titre';
@@ -53,6 +54,22 @@ export class HomePage {
     }, (err) => {
       // Handle error
     });
+  }
+
+  getGeolocation() {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      resp.coords.latitude
+      resp.coords.longitude
+     }).catch((error) => {
+       console.log('Error getting location', error);
+     });
+     
+     let watch = this.geolocation.watchPosition();
+     watch.subscribe((data) => {
+      // data can be a set of coordinates, or an error (if an error occurred).
+      data.coords.latitude
+      data.coords.longitude
+     });
   }
 
 }
