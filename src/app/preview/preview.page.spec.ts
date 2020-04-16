2c029733c8d3c0374f95420d6745867565defee3
BAUD <alexandre.baud@ynov.com>
2020-04-16 11:16:19 +0200

    config badgle CI

--- src/app/preview/preview.page.spec.ts
+++ src/app/preview/preview.page.spec.ts
@@ -6,7 +6,24 @@ import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
 import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 import {LoginService} from '../services/login.service';
+import { CameraPreview } from '@ionic-native/camera-preview/ngx';
+import {RouterTestingModule} from '@angular/router/testing';
 
 describe('PreviewPage', () => {
-    
+
+    let component: PreviewPage;
+    let fixture: ComponentFixture<PreviewPage>;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+        declarations: [ PreviewPage ],
+        imports: [IonicModule.forRoot(), RouterTestingModule],
+        providers: [CameraPreview]
+        }).compileComponents();
+
+        fixture = TestBed.createComponent(PreviewPage);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    }));
+
 });
